26d061be2b1d97a30026567b71e0d887bb6327cf
Shane Walsh <swalsh818@gmail.com>
2023-10-06 10:41:51 +0100

    First commit

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+function Footer(){
+    return (
+        // footer component that displays a footer for bottom of page
+        <div>
+            <h2>My Footer in another component!</h2>
+        </div>
+    );
+    }
+    
+    export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+function Header(){
+return (
+    // header component that displays a header for top of page
+    <div>
+        <h2>My header from another component!</h2>
+    </div>
+);
+}
+
+export default Header;
